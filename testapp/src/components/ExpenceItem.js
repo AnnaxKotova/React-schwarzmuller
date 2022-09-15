52d9a08fd6db48836d54eb0f225c8936b7186291
Anna <a.kotova@tx-consulting.com>
2022-09-15 21:34:12 +0200

    feat: add component ExpenseItem

---
+++ testapp/src/components/ExpenceItem.js
@@ -0,0 +1,19 @@
+import './ExpenceItem.css'
+
+
+function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 295.74;
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
